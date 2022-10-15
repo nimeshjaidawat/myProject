f8a5cdd09a61c11daa12b9c9487ba24ba8f88175
tushar taneja <tushar.taneja61@gmail.com>
2022-10-15 17:06:50 +0530

    15102022 tushar

--- src/app/styleguide/styleguide.component.ts
+++ src/app/styleguide/styleguide.component.ts
@@ -4,6 +4,8 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-styleguide',
